077a3b9ba9549bcd6948da8b932f778625620e35
harry <hung.k@shopstack.asia>
2019-02-21 13:40:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <p>
+  //           Edit <code>src/App.js</code> and save to reload.
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   );
+  // }
+
+  render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="custom-top">
+          <div><img width="200" src={logo} className="App-logo" alt="logo" /></div>
+          <div className="top-text">Chao Mung Den Voi Khoa Hoc ReactJs</div>
+        </div>
+        <div className="custom-bottom">De bat dau hoc vui long truy cap <code>src/App.js</code> de chinh sua</div>
       </div>
+      
     );
   }
 }
