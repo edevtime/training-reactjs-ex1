44616c7d093ad360e3178117fe78110755681d43
harry <hung.k@shopstack.asia>
2019-02-28 13:59:37 +0700

    Updates

---
+++ precache-manifest.908a181324084d1e21d677ff4f30a095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/training-reactjs-ex1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "492766c2432d9591e50d",
+    "url": "/training-reactjs-ex1/static/js/runtime~main.492766c2.js"
+  },
+  {
+    "revision": "d7c4ea2496e7e81d6d45",
+    "url": "/training-reactjs-ex1/static/js/main.d7c4ea24.chunk.js"
+  },
+  {
+    "revision": "3f240d0c9fc40074016c",
+    "url": "/training-reactjs-ex1/static/js/2.3f240d0c.chunk.js"
+  },
+  {
+    "revision": "d7c4ea2496e7e81d6d45",
+    "url": "/training-reactjs-ex1/static/css/main.e362fe5c.chunk.css"
+  },
+  {
+    "revision": "57d8541e9a0b049bbb0ae87fa13b5acc",
+    "url": "/training-reactjs-ex1/index.html"
+  }
+];
